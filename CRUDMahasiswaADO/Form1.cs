37c1ebc695fa53ca33701d033639c6f303fcadf3
MuhammadRioAprilianto <rma994636@gmail.com>
2026-04-09 17:20:59 +0700

    Menambahkan Namespace

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace CRUDMahasiswaADO
@@ -15,6 +9,11 @@ namespace CRUDMahasiswaADO
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
